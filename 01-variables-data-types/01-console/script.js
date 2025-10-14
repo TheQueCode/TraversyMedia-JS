a65f28e9bb2f59b391a5f5d92c6d8b5b7b0ffafd
TheQueCode <thequecode@gmail.com>
2025-10-14 17:42:11 -0500

    Section 1 - Lesson 1 - Console

---
+++ 01-variables-data-types/01-console/script.js
@@ -0,0 +1,20 @@
+console.log(110);
+
+console.log('Hello World');
+
+console.log(20, 'This is a string', true);
+
+const x = 100;
+
+console.log(x);
+
+console.table({name: 'Que', email: 'que@gmail.com'});
+
+console.group('simple');
+console.error('Alert');
+console.warn('Warning');
+console.groupEnd();
+
+const styles = 'padding: 10px; background-color: white; color: green';
+
+console.log('%cHello World', styles);
