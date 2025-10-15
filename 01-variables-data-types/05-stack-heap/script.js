120fe1548f2a613a0ebba373ba7de28a31c3a187
TheQueCode <thequecode@gmail.com>
2025-10-15 17:29:14 -0500

    Section 1 - Lesson 5 - Stack vs Heap

---
+++ 01-variables-data-types/05-stack-heap/script.js
@@ -0,0 +1,20 @@
+// Primitive values stored on the stack
+
+const name = 'John';
+const age = 30;
+
+// Reference values stored on the heap
+
+const person = {
+    name: 'Que',
+    age: 40
+}
+
+let newName = name;
+newName = 'Johnathan';
+
+let newPerson = person;
+newPerson.name = 'Doe';
+
+console.log(name, newName);
+console.log(person, newPerson);
