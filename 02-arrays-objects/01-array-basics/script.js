72b26199766031c7d2f15483d280cb767b0b3f11
TheQueCode <thequecode@gmail.com>
2025-10-16 17:47:46 -0500

    Section 2 - Lesson 1 - Array Basics

---
+++ 02-arrays-objects/01-array-basics/script.js
@@ -0,0 +1,23 @@
+let x;
+
+// Array Literal
+
+const numbers = [12, 45, 33, 29, 39, 110];
+
+// Array Constructor
+
+const fruits = new Array('apple', 'grape', 'orange');
+
+x = numbers[0];
+
+x = numbers.length;
+
+fruits[1] = 'pear';
+
+fruits[fruits.length] = 'blueberry';
+
+x = fruits;
+
+
+
+console.log(x);
