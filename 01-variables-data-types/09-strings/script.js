733f2dae68b8bab32b35bf035e73c1fea76846f1
TheQueCode <thequecode@gmail.com>
2025-10-15 22:34:37 -0500

    Section 1 - Lesson 9 - Strings

---
+++ 01-variables-data-types/09-strings/script.js
@@ -0,0 +1,46 @@
+let x;
+
+const name = 'Que';
+const age = 30;
+
+// Template Literals (Template Strings)
+x = `Hello, my name is ${name} and I am ${age} years old.`
+
+// String Properties and Methods
+
+const s = 'Hello World';
+
+x = s.length;
+
+// Access value by key
+x = s[0];
+
+x = s.__proto__;
+
+x = s.toUpperCase();
+x = s.toLowerCase();
+
+x = s.charAt(0);
+
+x = s.indexOf('H');
+
+x = s.substring(1, 5);
+x = s.substring(7);
+
+x = s.slice(-11, -6);
+
+x = s.trim()
+
+x = s.replace('World', 'Que');
+
+x = s.includes('Hel')
+
+x = s.valueOf();
+
+x = s.split('');
+
+
+
+
+
+console.log(x);
