7f7a8fb1365ce2f19f1c42553f4360583b6076ab
TheQueCode <thequecode@gmail.com>
2025-10-16 21:12:10 -0500

    Section 2 - Lesson 4 - Array Challenge

---
+++ 02-arrays-objects/04-array-challenge/script.js
@@ -0,0 +1,18 @@
+const arr = [1, 2, 3, 4, 5];
+
+arr.push(6);
+arr.unshift(0);
+arr.reverse();
+
+console.log(arr);
+
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 6, 7, 8, 9, 10];
+
+const arr3 = arr1.slice(0,4).concat(arr2);
+const anotherArr = arr1.splice(0, 4);
+const arr4 = [...anotherArr, ...arr2]
+
+console.log(arr3);
+console.log(arr4);
